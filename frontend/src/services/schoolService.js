e70560ea3a6c8c76ce5742974779c4d4fdefe180
anshul-636 <anshul-636@users.noreply.github.com>
2026-05-20 17:57:47 +0530

    frontend: implement school profiles api

---
+++ frontend/src/services/schoolService.js
@@ -0,0 +1,11 @@
+import api from "./api";
+const schoolService = {
+  getAll: async () => { const res = await api.get("/schools"); return res.data; },
+  getById: async (id) => { const res = await api.get("/schools/" + id); return res.data; },
+  create: async (data) => { const res = await api.post("/schools", data); return res.data; },
+  update: async (id, data) => { const res = await api.put("/schools/" + id, data); return res.data; },
+  getMySchool: async () => { const res = await api.get("/schools/my/school"); return res.data; },
+  delete: async (id) => { const res = await api.delete("/schools/" + id); return res.data; },
+  adminCreate: async (data) => { const res = await api.post("/schools/admin-create", data); return res.data; },
+};
+export default schoolService;
