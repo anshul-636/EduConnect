59370ae503f0eef02d678edf6cd26587deabe888
anshul-636 <anshul-636@users.noreply.github.com>
2026-05-20 17:57:47 +0530

    frontend: implement library resource api

---
+++ frontend/src/services/resourceService.js
@@ -0,0 +1,14 @@
+import api from './api';
+
+const resourceService = {
+  getAll: async (params) => { const res = await api.get('/resources', { params }); return res.data; },
+  getById: async (id) => { const res = await api.get('/resources/' + id); return res.data; },
+  upload: async (formData) => {
+    const res = await api.post('/resources', formData, { headers: { 'Content-Type': 'multipart/form-data' } });
+    return res.data;
+  },
+  delete: async (id) => { const res = await api.delete('/resources/' + id); return res.data; },
+  upvote: async (id) => { const res = await api.post('/resources/' + id + '/upvote'); return res.data; },
+};
+
+export default resourceService;
